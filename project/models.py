eae2eb110f78c4e5426c193231cb0f021da4be53
richardkefa <richardkefa@gmail.com>
2020-10-24 23:12:01 +0300

    add project modell

--- project/models.py
+++ project/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import dt
+from django.contrib.auth.models import User
+from django.urls import reverse 
 
-# Create your models here.
+class Projects(models.Model):
+  project_title = models.CharField(max_length=100)
+  landing_page_image = models.ImageField(upload_to='images/')
+  description = models.TextField()
+  repo_link = models.CharField(max_length=200)
+  live_link = models.CharField(max_length=500)
+  user = models.ForeignKey(User,on_delete=models.CASCADE,default='1')
+  
+
+
