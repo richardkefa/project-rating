46f3a54b3a886f264775dee7de8296c316f7d403
richardkefa <richardkefa@gmail.com>
2020-10-24 11:11:40 +0300

    add register view function

---
+++ user/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .forms import UserRegistrationForm
+
+def register(request):
+  if request.method == 'POST':
+    form = UserRegistrationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      username = form.cleaned_data.get('username')
+      messages.success(request,f'Your account has been created successfuly')
+      return redirect('login')
+  else:
+    form = UserRegistrationForm()
+    
+  return render(request,'user/register.html',{"form":form})
