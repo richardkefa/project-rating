8f2cccfec6926d1d472f9be91136b36d33999164
richardkefa <richardkefa@gmail.com>
2020-10-25 02:23:21 +0300

    add projects model class

--- project/views.py
+++ project/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse,Http404
+from .models import Projects,Ratings
+from django.views.generic import ListView,CreateView,UpdateView,DetailView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
